bc2314d12e1689e363da35cafd8930d4a522ddf3
Kirsten <kirsten.staggs@gmail.com>
2022-06-14 21:14:57 -0600

    commit tree c5cffbb4692f5bc6a151534d071c4a91a2f5c638

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
+private
+  def render_not_found(error)
+    render json: { error: error.message}, status: :not_found
+  end
+
+  def render_invalid(error)
+    render json: {errors: error.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
